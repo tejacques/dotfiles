2faab24516460d891e8eeb2f8c380938803c2be3
Tom Jacques <tejacques@gmail.com>
2015-04-10 14:58:13 -0400

    Improvements

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,37 +9,71 @@ function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;33m➜\033[0m  $@"; }
 
+function setup_bin() {
+    BINDIR="$HOME/bin"
+    e_arrow "Checking $BINDIR"
+    if [[ ! -e "$BINDIR" ]]
+    then
+        e_arrow mkdir -p "$BINDIR"
+        mkdir -p "$BINDIR"
+        e_arrow chmod +x $BINDIR
+        chmod +x $BINDIR
+    fi
+    if [[ ! -e "$BINDIR" ]]
+    then
+        e_error "Error: bin directory not created"
+    fi
+}
+
 function backup_file() {
     local base=$1
     local dest=$2
+    local msg="cp $dest $backup_dir"
     e_arrow "Backing up ~/$base."
     # Set backup flag, so a nice message can be shown at the end.
     backup=1
     # Create backup dir if it doesn't already exist.
-    if [[ -ne "$backup_dir" ]]
+    if [[ ! -e "$backup_dir" ]]
     then
         e_arrow "Making backup directory: $backup_dir"
         mkdir -p "$backup_dir"
     fi
+    if [[ ! -e "$backup_dir" ]]
+    then
+        e_error "Error backup directory not created"
+    fi
     # Backup file / link / whatever.
-    echo mv "$dest" "$backup_dir"
-    mv "$dest" "$backup_dir"
+    if cp -r "$dest" "$backup_dir"
+    then
+        e_success $msg
+    else
+        e_error $msg
+    fi
 }
 
 function copy_file() {
     local base=$1
     local dest=$2
-    e_success "Copying ~/$base"
-    echo cp "$dest" ~/
-    cp "$dest" ~/
+    local msg="cp -r $dest ~/"
+    if cp -r "$dest" ~/
+    then
+        e_success $msg
+    else
+        e_error $msg
+    fi
 }
 
 function link_file() {
     local base=$1
     local dest=$2
-    e_success "Linking ~/$base"
-    echo ln -sf ${dest#$HOME/} ~/
-    ln -sf ${dest#$HOME/} ~/
+    local msg="echo ln -sf ${dest#$HOME/} ~/"
+    
+    if ln -sf ${dest#$HOME/} ~/
+    then
+        e_success $msg
+    else
+        e_error $msg
+    fi
 }
 
 function do_stuff() {
@@ -47,9 +81,7 @@ function do_stuff() {
     local cmd=$1
     local dir=$2
     local files=(~/dotfiles/$dir/*)
-    local files2=~/dotfiles/$dir
 
-    echo files: $files
     if (( ${#files[@]} == 0 )); then return; fi
 
     for file in "${files[@]}"; do
@@ -75,13 +107,20 @@ backup_dir="$HOME/dotfiles/backups/$(date "+%Y_%m_%d-%H_%M_%S")/"
 
 echo ""
 echo ""
+
+#setup_bin
+
+echo '=============================='
 echo 'do_stuff "copy" "copy"'
+echo '=============================='
 do_stuff "copy" "copy"
 
 
 echo ""
 echo ""
+echo '=============================='
 echo 'do_stuff "link" "link"'
+echo '=============================='
 do_stuff "link" "link"
 
 
